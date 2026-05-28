e013ad0eae11552dfd43b4b9bb7bada8e9e0e714
Qaulan Tsaqila <qaulantsaqila75@gmail.com>
2026-05-28 19:40:20 +0700

    fix koneksi database ke tidb cloud

--- koneksi.php
+++ koneksi.php
@@ -1,22 +1,13 @@
 <?php
-$host_koneksi = "localhost";
-$username_koneksi = "root";
-$password_koneksi = "";
-$database_koneksi = "angkatan3_laundry";
+$host_koneksi = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com";
+$username_koneksi = "3geAjnUBSzaUMDi.root";
+$password_koneksi = "LDISXMaHdZQT0Lo5";
+$database_koneksi = "laundry_db";
 
-$koneksi = mysqli_connect($host_koneksi, $username_koneksi, $password_koneksi,  $database_koneksi);
+// Tambahkan port 4000 di fungsi mysqli_connect karena TiDB tidak pakai port default 3306
+$koneksi = mysqli_connect($host_koneksi, $username_koneksi, $password_koneksi, $database_koneksi, 4000);
 
 if (!$koneksi) {
-    echo "Koneksi Gagal";
+    die("Koneksi Gagal: " . mysqli_connect_error());
 }
-
-// $host_koneksi = "sql304.infinityfree.com";
-// $username_koneksi = "if0_42013691";
-// $password_koneksi = "WKmuIDdJXPZM";
-// $database_koneksi = "if0_42013691_angkatan3_laundry";
-
-// $koneksi = mysqli_connect($host_koneksi, $username_koneksi, $password_koneksi,  $database_koneksi);
-
-// if (!$koneksi) {
-//     die("Koneksi Gagal: " . mysqli_connect_error());
-// }
+?>
