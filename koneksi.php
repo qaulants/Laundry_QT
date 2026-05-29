695bea170bf124196d06503028c229c25791f70f
Qaulan Tsaqila <qaulantsaqila75@gmail.com>
2026-05-29 22:35:29 +0700

    update koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,11 @@
 <?php
-$host_koneksi = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com";
-$username_koneksi = "3geAjnUBSzaUMDi.root";
-$password_koneksi = "LDISXMaHdZQT0Lo5";
-$database_koneksi = "laundry_db";
+$host_koneksi = "localhost";
+$username_koneksi = "root";
+$password_koneksi = "";
+$database_koneksi = "angkatan3_laundry";
 
-// Tambahkan port 4000 di fungsi mysqli_connect karena TiDB tidak pakai port default 3306
-$koneksi = mysqli_connect($host_koneksi, $username_koneksi, $password_koneksi, $database_koneksi, 4000);
+$koneksi = mysqli_connect($host_koneksi, $username_koneksi, $password_koneksi,  $database_koneksi);
 
 if (!$koneksi) {
-    die("Koneksi Gagal: " . mysqli_connect_error());
+    echo "Koneksi Gagal";
 }
-?>
